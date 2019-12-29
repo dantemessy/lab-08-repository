71b88f3f488a2b917e2591aa4d039168fec3f0af
alaamahasneh <alaa.ibraheem97@yahoo.com>
2019-12-29 11:40:28 +0200

    edit schema

--- schema.sql
+++ schema.sql
@@ -9,28 +9,3 @@ CREATE TABLE information (
     longitude VARCHAR(255)
 
 );
-CREATE table weathers (
-    id serial primary key,
-    forecast varchar(255),
-    time varchar(255)
-);
-
-CREATE table yelps (
-    id serial primary key,
-    name varchar(255),
-    image_url varchar(255),
-    price int,
-    rating numeric(2,1),
-    url varchar(255)
-);
-
-CREATE table movies (
-    id serial primary key,
-    title varchar(255),
-    overview varchar(255),
-    average_votes numeric (8,2),
-    total_votes numeric(8,0),
-    image_url varchar(255),
-    popularity numeric(6,4),
-    released_on varchar(255)
-);
--- server.js
+++ server.js
@@ -104,6 +104,7 @@ function dataBaseLocation(city, data) {
 
 
 // first step , just to make our code cleaner
+// first step , just to make our code cleaner
 server.get('/weather', weatherHandler);
 
 // the costructor function
@@ -113,49 +114,26 @@ function Weather(day) {
 }
 
 function weatherHandler(request, response) {
-  let sql = `INSERT INTO weathers (forecast, time) VALUES($1, $2)`;
   let lat = request.query['latitude'];
   let lng = request.query['longitude'];
-  return client.query(sql,lat,lng)
-  .then(results => {
-  if (results.rowCount) {
-    return results.rows[0];
-  } else {
-    const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${lng}`;
-    return superagent.get(url)
+  getWeatherData(lat, lng)
+    .then((data) => {
+      response.status(200).send(data);
+    });
 
-          .then(data => dataBaseWeather(lat, lng));
-      }
+}
+
+function getWeatherData(lat, lng) {
+  const url = `https://api.darksky.net/forecast/${DARKSKY_API_KEY}/${lat},${lng}`;
+  return superagent.get(url)
+    .then((weatherData) => {
+      //   console.log(weatherData.body.daily.data);
+      let weather = weatherData.body.daily.data.map((day) => new Weather(day));
+      return weather;
     });
 }
-function dataBaseLocation(lat, lng) {
-  let weather = weatherData.body.daily.data.map((day) => new Weather(day));
-  let SQL = `
-  INSERT INTO information (forcast, time) 
-  VALUES ($1, $2) 
-  RETURNING *
-`;
-let values = [weather.forecast, weather.time];
-  return client.query(SQL,values)
-    .then(results => results.rows[0]);
-}
 
-//   getWeatherData(lat, lng)
-//     .then((data) => {
-//       response.status(200).send(data);
-//     });
-
-// }
-
-// function getWeatherData(lat, lng) {
-//   return superagent.get(url)
-//     .then((weatherData) => {
-//       //   console.log(weatherData.body.daily.data);
-//       let weather = weatherData.body.daily.data.map((day) => new Weather(day));
-//       return weather;
-//     });
-// }
-
+/////////////////////////// test
 /////////////////////////// test
 
 server.get('/events', eventHandler);
